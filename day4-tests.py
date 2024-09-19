1a6b7beb9fb4650a7731b9bae94f8f557829167f
kittyroyal <60828115+kittyroyal@users.noreply.github.com>
2024-09-19 14:16:01 -0700

    Update day4-tests.py
    done

--- day4-tests.py
+++ day4-tests.py
@@ -1,1 +1,21 @@
-fghjk
+grade = 100
+while grade <200:
+	Grade = grade +20
+	Print (grade)
+
+students = ['Ana' , 'Miriam' , 'Valeria' , 'Alison' , 'Beyonce'
+            for 1 in students:
+              print('Hi, '+ i)
+
+            print(students)
+
+            student_grade=[]
+            for i in students:
+            student_grade.append(100)
+
+            print(student_grade)
+            
+            
+
+                    
+
